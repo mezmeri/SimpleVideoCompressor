a49abe94088a4e6e23ae4bc50c19099800610ffe
Mads Schmidt Andersen <78485659+mezmeri@users.noreply.github.com>
2024-08-04 16:07:54 +0200

    • Added some click events • Added basic MVC structure

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data;
 using System.Windows;
 
-namespace SimpleEncoder
+namespace SimpleVideoCompressor
 {
     /// <summary>
     /// Interaction logic for App.xaml
---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using SimpleVideoCompressor.Controllers;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleVideoCompressor
+{
+    public partial class MainWindow : Window
+    {
+        private MainWindowController controller;
+        public MainWindow()
+        {
+            InitializeComponent();
+            controller = new();
+            DataContext = controller;
+        }
+
+        private void btn_FileUploadUser_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+
+            bool? dialogResult = dialog.ShowDialog();
+            if (dialogResult == true)
+            {
+                textblock_UserFile.Text = dialog.SafeFileName;
+            }
+        }
+
+        private void btn_FilePathUser_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.OpenFolderDialog();
+
+            bool? dialogResult = dialog.ShowDialog();
+            if (dialogResult == true)
+            {
+                textblock_UserFilePath.Text = dialog.FolderName;
+            }
+        }
+    }
+}
